bb0982c78f6b789f1b3a30bd30f4290a5c25fc60
PaquitoSoft <paquitosoftware@gmail.com>
2022-02-27 21:31:30 +0100

    docs: added API code blocks

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,3 +1,8 @@
+/**
+ * Fetcher is a small library on top of native [fetch API](https://developer.mozilla.org/en-US/docs/Web/API/fetch) to make it easier
+ * working with network requests.
+ * @module
+ */
 import getData from "./get-data";
 import InMemoryCache from "./in-memory-cache";
 import { sendData } from "./send-data";
@@ -11,15 +16,32 @@ export type {
 
 let cache: CacheManager = InMemoryCache;
 
+/**
+ * Sets the [[CacheManager]] to use when using ttl in [[get]] requests.
+ * By default **Fetcher** will use an in-memory cache.
+ * @param manager Cache manager to use
+ */
 export function setCacheManager(manager: CacheManager): void {
   cache = manager;
 }
 
+/**
+ * Represents the options you may use when using the different request types.
+ * @param ttl Time to live in seconds
+ * @param fetchOptions The [options](https://microsoft.github.io/PowerBI-JavaScript/interfaces/_node_modules_typedoc_node_modules_typescript_lib_lib_dom_d_.requestinit.html) to pass to the native [fetch API](https://developer.mozilla.org/en-US/docs/Web/API/fetch#parameters)
+ */
 export type RequestOptions = {
   ttl?: number; // seconds
   fetchOptions?: RequestInit;
 };
 
+/**
+ * Send a GET request to the given URL.
+ * @param url
+ * @param options
+ * @typeParam T Type of the data to return
+ * @returns The fetched data (response body)
+ */
 export function get<T>(url: string, options?: RequestOptions) {
   return getData<T>(url, {
     ttl: options?.ttl,
@@ -28,14 +50,37 @@ export function get<T>(url: string, options?: RequestOptions) {
   });
 }
 
+/**
+ * Send a POST request to the given URL.
+ * @param url
+ * @param body
+ * @param options
+ * @typeParam T Type of the data to return
+ * @returns The fetched data (response body)
+ */
 export function post<T>(url: string, body: object | string, options?: RequestOptions) {
   return sendData<T>(url, { method: 'POST', body, fetchOptions: options?.fetchOptions });
 }
 
+/**
+ * Send a PUT request to the given URL.
+ * @param url
+ * @param body
+ * @param options
+ * @typeParam T Type of the data to return
+ * @returns The fetched data (response body)
+ */
 export function put<T>(url: string, body: object | string, options?: RequestOptions) {
   return sendData<T>(url, { method: 'PUT', body, fetchOptions: options?.fetchOptions });
 }
 
+/**
+ * Send a DELETE request to the given URL.
+ * @param url
+ * @param options
+ * @typeParam T Type of the data to return
+ * @returns The fetched data (response body)
+ */
 export function del<T>(url: string, options?: RequestOptions & { body?: object | string }) {
   return sendData<T>(url, {
     method: 'DELETE',
--- src/in-memory-cache.ts
+++ src/in-memory-cache.ts
@@ -1,3 +1,8 @@
+/**
+ * In-memory cache manager.
+ * Cached values will be lost after the page is reloaded.
+ * @module
+ */
 import { CacheManager } from "./shared-types";
 
 type CacheEntry = {
@@ -8,6 +13,13 @@ type CacheEntry = {
 const _cache = new Map<string, CacheEntry>();
 
 const InMemoryCache: CacheManager = {
+  /**
+   * Returns the cached value for the given key if it exists.
+   * In case the value were cached with a ttl and the ttl has expired,
+   * the value will be removed from the cache and `undefined` will be returned.
+   * @param key
+   * @returns
+   */
   get(key: string): unknown {
     const cacheEntry = _cache.get(key);
     const now = Date.now();
@@ -20,6 +32,13 @@ const InMemoryCache: CacheManager = {
     }
   },
 
+  /**
+   * Stores the given value in the cache.
+   * If a ttl is given, the value is expected to be the **seconds** to live.
+   * @param key
+   * @param value
+   * @param options
+   */
   set(key: string, value: unknown, options?: { ttl?: number }): void {
     _cache.set(key, {
       value,
